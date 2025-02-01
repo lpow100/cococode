d3c341b47e521917f30671f47386ba85eb62f6fa
lpow100 <lgn.kocher@gmail.com>
2025-02-01 10:20:17 -0800

    okie

--- Assets/scripts/playerMove.cs
+++ Assets/scripts/playerMove.cs
@@ -1,16 +1,3 @@
-using UnityEngine;
-
-public class playerMove : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9dc722bbdf0d5e07ba2720c7d77778487a3635225e738f90743f2ce3e9099edc
+size 448
